be08072f3927d5ebb24ad31b95a3f8230963a98b
Matheus Rossetti <mh.rossetti2002@gmail.com>
2026-02-28 13:15:14 -0300

    mend

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use rand::{Rng, SeedableRng, rngs::StdRng};
+use std::hash::{DefaultHasher, Hash, Hasher};
 
-struct ActionError;
+use rand::{SeedableRng, rngs::StdRng};
 
-struct Entity {
+#[derive(Hash)]
+pub struct Entity {
     id: u32,
     stats: Stats,
     position: Position,
@@ -11,7 +12,7 @@ struct Entity {
 impl Entity {
     fn new(id: u32) -> Self {
         Self {
-            id: id,
+            id,
             stats: Stats {
                 hp: 15,
                 ap: 10,
@@ -22,73 +23,86 @@ impl Entity {
     }
 }
 
+#[derive(Hash)]
 struct Stats {
     hp: u32,
     ap: u32,
     max_ap: u32,
 }
 
+#[derive(Hash)]
 struct Position {
     x: u32,
     y: u32,
 }
 
-enum Action {
+pub enum Action {
+    Spawn,
     EndTurn,
+    Attack(u32),
 }
 
-struct GameState {
-    rng: StdRng,
-    next_entity_id: u32,
-    current_turn_id: u32,
-    entities: Vec<Entity>,
+pub struct GameState {
+    pub rng: StdRng,
+    pub next_entity_id: u32,
+    pub current_turn_index: usize,
+    pub entities: Vec<Entity>,
 }
 
 impl GameState {
-    fn new(seed: u64) -> Self {
+    pub fn new(seed: u64) -> Self {
         let mut game_state = Self {
             rng: SeedableRng::seed_from_u64(seed),
             next_entity_id: 1,
-            current_turn_id: 1,
+            current_turn_index: 0,
             entities: vec![],
         };
-        game_state.push_entity();
-        game_state.push_entity();
+        game_state.apply(Action::Spawn);
+        game_state.apply(Action::Spawn);
 
         game_state
     }
 
-    fn get_current_entity(&mut self) -> &mut Entity {
-        self.entities
-            .iter_mut()
-            .find(|x| x.id == self.current_turn_id)
-            .unwrap()
-    }
-
-    fn push_entity(&mut self) -> u32 {
-        let entity_id = self.next_entity_id;
-        self.next_entity_id += 1;
-
-        self.entities.push(Entity::new(entity_id));
-        entity_id
-    }
-
-    fn apply(&mut self, action: Action) -> Result<(), ActionError> {
+    pub fn apply(&mut self, action: Action) {
         match action {
-            Action::EndTurn => self.current_turn_id += 1,
-        }
+            Action::EndTurn => {
+                self.current_turn_index = (self.current_turn_index + 1) % self.entities.len()
+            }
+            Action::Spawn => {
+                let entity_id = self.next_entity_id;
+                self.next_entity_id += 1;
 
-        Ok(())
+                self.entities.push(Entity::new(entity_id));
+            }
+            Action::Attack(target_id) => {
+                let entity = self.get_entity_by_id(target_id).expect("Unvalid target");
+                entity.stats.hp -= 3;
+
+                let current = self.get_current().expect("unvalid current entity");
+                current.stats.ap -= 2;
+            }
+        }
     }
 
-    fn roll_u32(&mut self) -> u32 {
-        self.rng.next_u32()
+    fn get_entity_by_id(&mut self, entity_id: u32) -> Option<&mut Entity> {
+        self.entities.iter_mut().find(|x| x.id == entity_id)
+    }
+
+    fn get_current(&mut self) -> Option<&mut Entity> {
+        self.entities.get_mut(self.current_turn_index)
+    }
+
+    pub fn hash(&self) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        self.entities.hash(&mut hasher);
+        self.current_turn_index.hash(&mut hasher);
+        hasher.finish()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::vec;
+    use rand::Rng;
 
     use super::*;
 
@@ -97,7 +111,8 @@ mod tests {
         let mut left = GameState::new(123);
         let mut right = GameState::new(123);
 
-        assert_eq!(left.roll_u32(), right.roll_u32());
+        let all: bool = (0..20).all(|_| left.rng.next_u32() == right.rng.next_u32());
+        assert!(all);
     }
 
     #[test]
@@ -105,26 +120,48 @@ mod tests {
         let mut left = GameState::new(123);
         let mut right = GameState::new(321);
 
-        assert_ne!(left.roll_u32(), right.roll_u32());
+        let all: bool = (0..20).all(|_| left.rng.next_u32() == right.rng.next_u32());
+        assert!(!all);
     }
 
     #[test]
     fn push_entity_when_adding_entity_should_increase_next_id_by_one() {
         let mut state = GameState::new(123);
         let before = state.next_entity_id;
-        let new_entity_id = state.push_entity();
+        state.apply(Action::Spawn);
 
         assert_eq!(state.next_entity_id, before + 1);
-        assert_eq!(new_entity_id, before);
     }
 
     #[test]
     fn apply_when_current_end_turn_should_swap_next_entity() {
         let mut state = GameState::new(123);
-        let before = state.current_turn_id;
-        let _ = state.apply(Action::EndTurn);
+        let before = state.current_turn_index;
+        state.apply(Action::EndTurn);
 
-        let after = state.current_turn_id;
+        let after = state.current_turn_index;
         assert_ne!(before, after);
+    }
+
+    #[test]
+    fn hash_when_current_state_same_should_be_equal() {
+        let mut a = GameState::new(123);
+        let mut b = GameState::new(123);
+
+        a.apply(Action::EndTurn);
+        b.apply(Action::EndTurn);
+
+        assert_eq!(a.hash(), b.hash());
+    }
+
+    #[test]
+    fn hash_when_current_state_not_same_should_not_be_equal() {
+        let mut a = GameState::new(123);
+        let mut b = GameState::new(123);
+
+        a.apply(Action::EndTurn);
+        b.apply(Action::Attack(1));
+
+        assert_ne!(a.hash(), b.hash());
     }
 }
